c9f86816fc34ce982261bc2422e9c4c37ead6471
Yusufhoja <usufhozah@gmail.com>
2024-02-28 17:37:51 +0500

    js 3 dars

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("sizda hozir random sonlar chiqishni boshlaydi😉")
+let b= Math.round(Math.random() *99+1)
+alert (`
+random orqali chiqqan son: ${b} 
+`)
+alert("Biz random sonni 2 ga kopaytirmoqchi ba bolmoqchimiz!")
+let d = (b*2)
+let e =(b/2)
+let p= (b % 3)
+alert(`
+Random orqali chiqqan son: ${b}
+biz uni 2 ga kopaytirib shu son oldik: ${d}
+va shu sonni 2 ga bolib shu sonni oldik: ${e}
+va shu sonni 3 ga bolgandagi qoldiq: ${p}
+`)
+
